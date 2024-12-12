f1204523dd8a43e6ebc79485b9477e40927eabc6
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-12-12 18:18:36 +0700

    Change color scheme

--- app/src/main/java/laiss/pokemon/android/ui/theme/Theme.kt
+++ app/src/main/java/laiss/pokemon/android/ui/theme/Theme.kt
@@ -12,32 +12,56 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+    background = Base,
+    onBackground = Text,
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    surface = Surface1,
+    surfaceBright = Surface2,
+    surfaceContainer = Surface1,
+    surfaceContainerHigh = Surface2,
+    surfaceContainerHighest = Surface2,
+    surfaceContainerLow = Surface0,
+    surfaceContainerLowest = Surface0,
+    surfaceDim = Surface0,
+    surfaceVariant = Surface0,
+    surfaceTint = Surface0,
+    inverseSurface = Crust,
+    onSurface = Text,
+    onSurfaceVariant = Subtext0,
+    inverseOnSurface = Text,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Peach,
+    primaryContainer = Rosewater,
+    inversePrimary = Rosewater,
+    onPrimary = Mantle,
+    onPrimaryContainer = Mantle,
+
+    secondary = Surface2,
+    secondaryContainer = Overlay2,
+    onSecondary = Text,
+    onSecondaryContainer = Crust,
+
+    tertiary = Yellow,
+    tertiaryContainer = Lavender,
+    onTertiary = Text,
+    onTertiaryContainer = Crust,
+
+    outline = Lavender,
+    outlineVariant = Overlay0,
+
+    scrim = MantleHalfOpacity,
+
+    error = Red,
+    errorContainer = Pink,
+    onError = Text,
+    onErrorContainer = Mantle,
 )
 
 @Composable
 fun PokemonAndroidTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -47,7 +71,7 @@ fun PokemonAndroidTheme(
         }
 
         darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> DarkColorScheme  // TODO: Add light color scheme
     }
 
     MaterialTheme(
