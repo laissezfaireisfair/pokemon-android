8421a1245116d04538c22c9d41b5c87a2b1bc811
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-12-17 15:32:24 +0700

    Move DTOs to dedicated file

---
+++ app/src/main/java/laiss/pokemon/android/data/dataSources/PokeApiDtos.kt
@@ -0,0 +1,35 @@
+package laiss.pokemon.android.data.dataSources
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PokemonHeaderDto(val name: String)
+
+@Serializable
+data class PokemonHeadersListDto(val count: Int, val results: List<PokemonHeaderDto>)
+
+@Serializable
+data class PokemonSpritesDto(val front_default: String?)
+
+@Serializable
+data class PokemonTypeDto(val name: String)
+
+@Serializable
+data class PokemonTypesDto(val type: PokemonTypeDto)
+
+@Serializable
+data class PokemonStatDto(val name: String)
+
+@Serializable
+data class PokemonStatsDto(val base_stat: Int, val stat: PokemonStatDto)
+
+@Serializable
+data class PokemonDto(
+    val name: String,
+    val id: Int,
+    val height: Int,
+    val weight: Int,
+    val sprites: PokemonSpritesDto,
+    val types: List<PokemonTypesDto>,
+    val stats: List<PokemonStatsDto>
+)
